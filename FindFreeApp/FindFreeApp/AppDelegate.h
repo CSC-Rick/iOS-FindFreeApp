8e5236008bdbb754fc497387e33ac79cd49d37a1
Rick <rxu4@csc.com>
2012-02-09 15:26:44 +0800

    Project init

---
+++ FindFreeApp/FindFreeApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FindFreeApp
+//
+//  Created by rickx on 2/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
